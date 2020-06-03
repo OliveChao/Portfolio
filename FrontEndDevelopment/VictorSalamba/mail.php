3bc10ac477b1b9dedc6289414237b1924e083354
anabangi <alvaronabangi@gmail.com>
2020-06-03 14:37:13 +0300

    submit form without page refresh with ajax and php!

--- FrontEndDevelopment/VictorSalamba/mail.php
+++ FrontEndDevelopment/VictorSalamba/mail.php
@@ -1,12 +1,27 @@
 <?php
+if ( ($_POST['name']!="") && ($_POST['email']!="")){
 $name = $_POST['name'];
-$number = $_POST['number'];
+// $number = $_POST['number'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "me@victorsalamba.co.ke";
-$subject = "Contact Form";
+$subject = "Website Contact Form";
+// Always set content-type when sending HTML email
+$mailheader = "MIME-Version: 1.0" . "\r\n";
+$mailheader = "Content-type:text/html;charset=UTF-8" . "\r\n";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+$sent = mail($recipient, $subject, $formcontent, $mailheader);
+if($sent){
+ echo "<span style='color:green; font-weight:bold;'>
+ Thank you for contacting me, I will get back to you shortly.
+ </span>";
+}
+else{
+ echo "<span style='color:red; font-weight:bold;'>
+ Sorry! Your form submission has failed!
+ </span>";
+ }
+}
+// echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
